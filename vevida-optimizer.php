745c50c3e75360e1b4f6ff6e96b0cb6e02e75188
TimVevida <timhavinga@vevida.com>
2019-08-19 16:33:29 +0200

    Remove extra slash in the stylesheet URL.

--- vevida-optimizer.php
+++ vevida-optimizer.php
@@ -39,7 +39,7 @@ if( is_array( $plugins ) ) {
  * Load stylesheet for the optimizer options page.
  */
 function vevida_optimizer_style() {
-	wp_enqueue_style( 'style',  VEVIDAOPTIMIZERURL . "/css/style.css");
+	wp_enqueue_style( 'style',  VEVIDAOPTIMIZERURL . "css/style.css");
 }
 add_action( 'admin_enqueue_scripts', 'vevida_optimizer_style' );
 
